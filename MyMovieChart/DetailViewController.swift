01a2322ccc7496b8f3a7946a90c2131d95a185d6
domb <woon4910@gmail.com>
2021-05-11 22:30:39 +0900

    + activity indicator + WKNavigationDelegate 프로토콜 구현 + UIViewController 에 alert 함수 정의해서 웹뷰 오류시 나타나도록 처리

--- MyMovieChart/DetailViewController.swift
+++ MyMovieChart/DetailViewController.swift
@@ -12,9 +12,14 @@ class DetailViewController: UIViewController {
     
     @IBOutlet var webView: WKWebView!
     
+    @IBOutlet weak var indicater: UIActivityIndicatorView!
+    
     var mvo: MovieVO!
     
     override func viewDidLoad() {
+        
+        self.webView.navigationDelegate = self
+        
         NSLog("\(self.mvo.detail!), \(self.mvo.title!)")
         
         let navibar = self.navigationItem
@@ -47,6 +52,50 @@ class DetailViewController: UIViewController {
             }
             alert.addAction(cancelAction)
             self.present(alert, animated: false, completion: nil)
+        }
+    }
+}
+//MARK: - WKNavigationDelegate 프로토콜 구현
+extension DetailViewController: WKNavigationDelegate {
+    // 웹뷰가 웹 페이지를 읽어올지 말지 결정 메소드
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        decisionHandler(.allow)
+        guard let url = navigationAction.request.url?.absoluteString else {
+            return
+        }
+        if (url.starts(with: "https://")) {
+            decisionHandler(.cancel)
+        }
+    }
+    
+    // 웹 뷰가 콘텐츠를 로드하기 시작할 때 호출되는 메소드 ! 아직 URL이 유효하지 않거나 온라인 상태일 때도 호출된다는 점에서 didCommit과 다르다.
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+    }
+    
+    // 웹 뷰카 콘텐츠를 받기 시작할 때 호출되는 메소드! 대표적으로 로딩상태 표시
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        self.indicater.startAnimating()
+    }
+    
+    // 웹 뷰가 콘텐츠 로딩을 완전히 마쳤을 때 호출되는 메소드!
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.indicater.stopAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        self.indicater.stopAnimating()
+        self.alert("상세페이지를 읽어오지 못했습니다.") {
+            _ = self.navigationController?.popViewController(animated: true)
+        }
+    }
+}
+extension UIViewController {
+    func alert(_ message: String, onClick: (() -> Void)? = nil) {
+        let controller = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        controller.addAction(UIAlertAction(title: "취소", style: .cancel) { (_) in onClick?()
+        })
+        DispatchQueue.main.async {
+            self.present(controller, animated: false)
         }
     }
 }
